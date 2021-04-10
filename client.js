62396c55988e909f3b5392480960d1733af69216
Norbert Schnell <norbert.schnell@hs-furtwangen.de>
2021-04-10 15:09:12 +0200

    minor update

--- client.js
+++ client.js
@@ -1,41 +1,39 @@
 "use strict";
-var helloWebSockets;
-(function (helloWebSockets) {
-    const socket = new WebSocket("ws://localhost:8000/");
-    const counterNames = [
-        "numClients",
-        "topLeft",
-        "topCenter",
-        "topRight",
-        "middleLeft",
-        "middleRight",
-        "bottomLeft",
-        "bottomCenter",
-        "bottomRight",
-    ];
-    for (let counterName of counterNames) {
-        const padDiv = document.querySelector(`#${counterName}`);
-        if (padDiv.id !== "numClients") {
-            padDiv.addEventListener("touchstart", (evt) => {
-                const div = evt.target;
-                socket.send(div.dataset.index);
-                div.classList.add("active");
-                setTimeout(() => div.classList.remove("active"), 100);
-                evt.preventDefault();
-            });
-        }
+// const socket: WebSocket = new WebSocket("ws://localhost:8000/");
+const socket = new WebSocket("wss://counter-pads.herokuapp.com/");
+const counterNames = [
+    "numClients",
+    "topLeft",
+    "topCenter",
+    "topRight",
+    "middleLeft",
+    "middleRight",
+    "bottomLeft",
+    "bottomCenter",
+    "bottomRight",
+];
+for (let counterName of counterNames) {
+    const padDiv = document.querySelector(`#${counterName}`);
+    if (padDiv.id !== "numClients") {
+        padDiv.addEventListener("touchstart", (evt) => {
+            const div = evt.target;
+            socket.send(div.dataset.index);
+            div.classList.add("active");
+            setTimeout(() => div.classList.remove("active"), 100);
+            evt.preventDefault();
+        });
     }
-    // listen to connection open
-    // socket.addEventListener("open", (event) => {
-    // });
-    // listen to message from server
-    socket.addEventListener("message", (event) => {
-        const counters = JSON.parse(event.data);
-        for (let i = 0; i < counters.length; i++) {
-            const counterName = counterNames[i];
-            const counterDiv = document.querySelector(`#${counterName} .counter`);
-            counterDiv.innerHTML = counters[i].toString();
-        }
-    });
-})(helloWebSockets || (helloWebSockets = {}));
+}
+// listen to connection open
+// socket.addEventListener("open", (event) => {
+// });
+// listen to message from server
+socket.addEventListener("message", (event) => {
+    const counters = JSON.parse(event.data);
+    for (let i = 0; i < counters.length; i++) {
+        const counterName = counterNames[i];
+        const counterDiv = document.querySelector(`#${counterName} .counter`);
+        counterDiv.innerHTML = counters[i].toString();
+    }
+});
 //# sourceMappingURL=client.js.map
--- client.ts
+++ client.ts
@@ -1,48 +1,47 @@
-namespace helloWebSockets { // name space to isolate identifiers from other examples
-  const socket: WebSocket = new WebSocket("ws://localhost:8000/");
+// const socket: WebSocket = new WebSocket("ws://localhost:8000/");
+const socket: WebSocket = new WebSocket("wss://counter-pads.herokuapp.com/");
 
-  const counterNames: string[] = [
-    "numClients",
-    "topLeft",
-    "topCenter",
-    "topRight",
-    "middleLeft",
-    "middleRight",
-    "bottomLeft",
-    "bottomCenter",
-    "bottomRight",
-  ];
+const counterNames: string[] = [
+  "numClients",
+  "topLeft",
+  "topCenter",
+  "topRight",
+  "middleLeft",
+  "middleRight",
+  "bottomLeft",
+  "bottomCenter",
+  "bottomRight",
+];
 
-  for (let counterName of counterNames) {
-    const padDiv: HTMLDivElement = document.querySelector(`#${counterName}`);
+for (let counterName of counterNames) {
+  const padDiv: HTMLDivElement = document.querySelector(`#${counterName}`);
 
-    if (padDiv.id !== "numClients") {
-      padDiv.addEventListener("touchstart", (evt) => {
-        const div: HTMLDivElement = <HTMLDivElement>evt.target;
-        socket.send(div.dataset.index);
+  if (padDiv.id !== "numClients") {
+    padDiv.addEventListener("touchstart", (evt) => {
+      const div: HTMLDivElement = <HTMLDivElement>evt.target;
+      socket.send(div.dataset.index);
 
-        div.classList.add("active");
+      div.classList.add("active");
 
-        setTimeout(() => div.classList.remove("active"), 100);
+      setTimeout(() => div.classList.remove("active"), 100);
 
-        evt.preventDefault();
-      });
-    }
+      evt.preventDefault();
+    });
   }
-
-  // listen to connection open
-  // socket.addEventListener("open", (event) => {
-  // });
-
-  // listen to message from server
-  socket.addEventListener("message", (event) => {
-    const counters: number[] = JSON.parse(event.data);
-
-    for (let i: number = 0; i < counters.length; i++) {
-      const counterName: string = counterNames[i];
-      const counterDiv: HTMLDivElement = document.querySelector(`#${counterName} .counter`);
-
-      counterDiv.innerHTML = counters[i].toString();
-    }
-  });
 }
+
+// listen to connection open
+// socket.addEventListener("open", (event) => {
+// });
+
+// listen to message from server
+socket.addEventListener("message", (event) => {
+  const counters: number[] = JSON.parse(event.data);
+
+  for (let i: number = 0; i < counters.length; i++) {
+    const counterName: string = counterNames[i];
+    const counterDiv: HTMLDivElement = document.querySelector(`#${counterName} .counter`);
+
+    counterDiv.innerHTML = counters[i].toString();
+  }
+});
